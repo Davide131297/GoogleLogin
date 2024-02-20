c6ae9615b307a1c1852e022cabdbf6431cadd577
davidechiffi <davide.chiffi@iu-study.org>
2024-02-20 15:40:00 +0100

    Microsoft Login erstellt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+import { useMsal } from "@azure/msal-react";
+import { loginRequest } from "./Config";
 
 const App = () => {
 
-  const [user, setUser] = useState({});
-  const [visible, setVisible] = useState(true);
+  const [user, setUser] = useState({}); //Google
+  const [visible, setVisible] = useState(true); //Google
+  const { instance } = useMsal(); //Microsoft
 
+  //Anmelden bei Google
   function handleCallBackResponse(response) {
     console.log("Encoded JWT ID token: " + response.credential);
     const userObject = jwtDecode(response.credential);
@@ -28,10 +34,40 @@ const App = () => {
     );
   }, []);
 
+  //Anmelden bei Microsoft
+  const handleLogin = (loginType) => {
+    if (loginType === "popup") {
+      instance.loginPopup(loginRequest).catch((e) => {
+        console.log(e);
+      });
+    } else if (loginType === "redirect") {
+      instance.loginRedirect(loginRequest).catch((e) => {
+        console.log(e);
+      });
+    }
+  };
+
   return (
     <div className="App">
-      {visible && <div id="signInDiv"></div>}
-      {!visible && <h1>Erfolgreich Angemeldet, {user}!</h1>}
+      <div className="Google Login">
+        {visible && <div id="signInDiv"></div>}
+        {!visible && <h1>Erfolgreich bei Google Angemeldet, {user}!</h1>}
+      </div>
+      <div className="Microsoft Login">
+        <DropdownButton
+          variant="secondary"
+          className="ml-auto"
+          drop="start"
+          title="Sign In"
+        >
+          <Dropdown.Item as="button" onClick={() => handleLogin("popup")}>
+            Sign in using Popup
+          </Dropdown.Item>
+          <Dropdown.Item as="button" onClick={() => handleLogin("redirect")}>
+            Sign in using Redirect
+          </Dropdown.Item>
+        </DropdownButton>
+      </div>
     </div>
   );
 };
